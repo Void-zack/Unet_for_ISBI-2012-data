d1ec1c8e3ed1b930c5f10b7b154424b80f4b6edf
ZACKLDHGZ <40418015+ZACKLDHGZ@users.noreply.github.com>
2019-12-07 13:27:56 +0800

    a

--- data_compare.py
+++ data_compare.py
@@ -1,6 +1,5 @@
 import os
 import numpy as np
-import configparser
 import sys
 import matplotlib
 matplotlib.use('Agg')
@@ -8,30 +7,33 @@ import matplotlib.pyplot as plt
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 from skimage.io import imread
 from keras.models import load_model
+from utils.processing import prep_raw
 from utils.processing import prep_raw_plot
 from utils.processing import test_mask
 from utils.visualization import bina
 from utils.visualization import Compare_results
+from utils.read_config import read
 
 def main():
     ID=int(sys.argv[1])
     ########################################
     # Set up compare configuration
     ########################################
-    config = configparser.RawConfigParser()
-    config.read('config.txt')
-    test_path = config.get('data path','test_path')
-    dir_path = config.get('data path','result_dir')
-    rfile = config.get('task settings','raw_file')
-    mfile = config.get('task settings','mask_file')
+    config = read()
+    test_path = config['test_path']
+    dir_path = config['result_dir']
+    rfile = config['raw_file']
+    mfile = config['mask_file']
     ########################################
     # Predicting
     ########################################
-    raw = prep_raw_plot(imread(test_path+rfile)[ID][np.newaxis,:,:])
+    img = imread(test_path+rfile)[ID][np.newaxis,:,:]
+    raw = prep_raw(img)
+    raw_plot = prep_raw_plot(img)
     net = load_model(dir_path+'model.hdf5')
     pred_ = bina(np.squeeze(net.predict(raw)))
-    label_ = test_mask(imread(test_path+mfile))[ID]
-    raw_ = np.squeeze(raw)
+    label_ = test_mask(imread(test_path+mfile))[ID]    
+    raw_ = np.squeeze(raw_plot)
     ########################################
     # Comparing
     ########################################
